fbe5e927a081e7b8a8b5ceafc80cac6fb63ad476
abhis <abhis@DESKTOP-1R745G8>
2023-01-02 23:50:56 +0530

    Bean scopes examples

--- springcore/src/main/java/com/springcore/stereotype/Student.java
+++ springcore/src/main/java/com/springcore/stereotype/Student.java
@@ -3,9 +3,11 @@ package com.springcore.stereotype;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("Example")
+@Scope("prototype")
 public class Student {
 	
 	/* in this case it generated a bean with name equal to camelcase version of Class name
--- springcore/src/main/java/com/springcore/stereotype/Test.java
+++ springcore/src/main/java/com/springcore/stereotype/Test.java
@@ -9,8 +9,19 @@ public class Test {
 	{
 		ApplicationContext ap = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereoconfig.xml");
 		Student student = ap.getBean("Example",Student.class);
-		System.out.println(student.toString());
-		System.out.print(student.getPlaces());
+		
+//		System.out.println(student.toString());
+//		System.out.print(student.getPlaces());
+//		System.out.println(student.hashCode());
+		
+		Student student2 = ap.getBean("Example",Student.class);
+//		System.out.println(student2.hashCode());
+		
+		Teacher t = ap.getBean("teacher",Teacher.class);
+		System.out.println(t.hashCode());
+		
+		Teacher t1 = ap.getBean("teacher",Teacher.class);
+		System.out.println(t1.hashCode());
 	}
 }
 
